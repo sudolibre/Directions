24ea27a2d7804ee033a2f3ebb10b1d68e1d98329
sudolibre <optout.breaker@gmail.com>
2016-12-01 15:35:59 -0500

    Initial Commit

---
+++ Directions/Directions.h
@@ -0,0 +1,19 @@
+//
+//  Directions.h
+//  Directions
+//
+//  Created by Jonathon Day on 12/1/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Directions.
+FOUNDATION_EXPORT double DirectionsVersionNumber;
+
+//! Project version string for Directions.
+FOUNDATION_EXPORT const unsigned char DirectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Directions/PublicHeader.h>
+
+
